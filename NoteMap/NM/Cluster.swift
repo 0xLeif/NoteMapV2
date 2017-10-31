bd86c03904b7951b2c7eac74b1f9d6dfa28fc4c4
ambid17 <sbambrose@unomaha.edu>
2017-10-31 16:25:04 -0500

    Added calculated vars for Cluster to determine height and width

--- NoteMap/NM/Cluster.swift
+++ NoteMap/NM/Cluster.swift
@@ -11,13 +11,30 @@ import UIKit
 class Cluster: UIView {
 	var notes: [Note] = []
 	
- func findCenterPointForNotes() -> CGPoint {
-		let centerPoints = notes.map{ $0.center }
-		let numberOfPoints: CGFloat = CGFloat(centerPoints.count)
-		let (totalX, totalY) = (centerPoints.map{ $0.x }.reduce(0, +),
-		                        centerPoints.map{ $0.y }.reduce(0, +))
-		let (avgX, avgY) = (totalX / numberOfPoints,
-		                    totalY / numberOfPoints)
-		return CGPoint(x: avgX, y: avgY)
-	}
+    var centerPoint: CGPoint {
+        let centerPoints = notes.map{ $0.center }
+        let numberOfPoints: CGFloat = CGFloat(centerPoints.count)
+        let (totalX, totalY) = (centerPoints.map{ $0.x }.reduce(0, +),
+                                centerPoints.map{ $0.y }.reduce(0, +))
+        let (avgX, avgY) = (totalX / numberOfPoints,
+                            totalY / numberOfPoints)
+        return CGPoint(x: avgX, y: avgY)
+    }
+    
+    var sizeForNotes: CGSize {
+        let bigger = max(currentWidth, currentHeight)
+        return CGSize(width: bigger, height: bigger)
+    }
+    
+    var currentWidth: CGFloat {
+        let currentCenter = centerPoint
+        let maxNoteDistance = notes.map{ abs($0.center.x - currentCenter.x) }.sorted(by: >).first ?? 0
+        return maxNoteDistance * 2
+    }
+    
+    var currentHeight: CGFloat {
+        let currentCenter = centerPoint
+        let maxNoteDistance = notes.map{ abs($0.center.y - currentCenter.y) }.sorted(by: >).first ?? 0
+        return maxNoteDistance * 2
+    }
 }
