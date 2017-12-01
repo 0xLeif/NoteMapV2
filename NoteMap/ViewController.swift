245d95ee40ad529c0c4cfd4e63266fb3a6486ba1
Zach Eriksen <zmeriksen@gmail.com>
2017-12-01 15:59:11 -0600

    Missed one!

--- NoteMap/ViewController.swift
+++ NoteMap/ViewController.swift
@@ -19,7 +19,6 @@ enum Colors {
 //let colorData = [UIColor.rgba(231, 76, 60,1.0), UIColor.rgba(230, 126, 34,1.0), UIColor.rgba(241, 196, 15,1.0), UIColor.rgba(46, 204, 113,1.0), UIColor.rgba(52, 152, 219,1.0), UIColor.rgba(122, 68, 193,1.0)]
 let colorData = [UIColor.rgba(193, 72, 72, 1), UIColor.rgba(224, 127, 79, 1), UIColor.rgba(239, 201, 76, 1), UIColor.rgba(104, 155, 109, 1), UIColor.rgba(76, 122, 137, 1), UIColor.rgba(145, 96, 140, 1)]
 
-let importanceData: [NoteImportance] = [.high, .mid, .low, .none]
 class ViewController: UIViewController {
 	@IBOutlet weak var noteMapScrollView: NoteMapScrollView!
 	let bottomSheet = BottomSheet(startingColor: colorData[0])
