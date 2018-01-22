71f391cf7c62743aae5ff57c7cacd58b3e16b128
ambid17 <sbambrose@unomaha.edu>
2018-01-21 20:01:00 -0600

    Fixed Merge Issue

--- NoteMap/AppDelegate.swift
+++ NoteMap/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RxSwift
 import Firebase
 
 @UIApplicationMain
@@ -29,6 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func applicationDidEnterBackground(_ application: UIApplication) {
 		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
 		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        SaveDataObservable.onNext(())
 	}
 
 	func applicationWillEnterForeground(_ application: UIApplication) {
@@ -41,6 +43,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func applicationWillTerminate(_ application: UIApplication) {
 		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        SaveDataObservable.onNext(())
 	}
 
 
