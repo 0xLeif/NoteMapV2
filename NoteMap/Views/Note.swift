5bf1a0be7cd104ac2999d75a08194ac454f4cdcb
Zach 'Leif' Eriksen <zeriksen@clientresourcesinc.com>
2018-05-14 19:41:21 -0500

    Added stupid init D:

--- NoteMap/Views/Note.swift
+++ NoteMap/Views/Note.swift
@@ -28,7 +28,11 @@ class Note: UITextView {
 		self.text = text
 		resizeText()
 	}
-	
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 	private func NMinit(atCenter point: CGPoint) {
         logAnalytic()
 		adjustsFontForContentSizeCategory = true
