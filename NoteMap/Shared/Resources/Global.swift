33db6ef964f72bc8083f97c301ebece80669598c
Zach 'Leif' Eriksen <zeriksen@clientresourcesinc.com>
2018-05-15 21:12:28 -0500

    Moved code to singleton and organized project files

--- NoteMap/Shared/Resources/Global.swift
+++ NoteMap/Shared/Resources/Global.swift
@@ -9,15 +9,10 @@
 import UIKit
 import RxSwift
 
-//MARK: Structs
+//MARK: Struct
 struct NMDefaults: Codable {
     var selectedColor: Color
     var selectedTheme: Theme
-}
-
-struct NMColor {
-    var color: Color
-    var uicolor: UIColor
 }
 
 //MARK: RX
@@ -25,10 +20,12 @@ var SaveDataObservable = PublishSubject<Void>()
 var LoadDataObservable = PublishSubject<String>()
 var selectedTheme: Variable<Theme> = Variable(.light)
 var selectedColor: Variable<Color> = Variable(.red)
-
-//MARK: Test Data
-let testTheme: [Color: UIColor] = [.red: .rgba(231, 76, 60,1.0)]
-let mockJsonData =
-"""
-{"clusters":[{"notes":[{"center":[18930.565034116524,38512.21754534565]},{"center":[17558.4949555535,39577.197808182646]},{"center":[18782.472960697447,42029.74474272153]},{"center":[20301.780579816335,40612.94883102019]},{"center":[17629.3239749151,41312.13303509432]}],"center":[18640.52750101978,40408.84839247287]},{"notes":[{"center":[21302.842702831767,42119.6648922478]},{"center":[20306.544466581738,45590.719979617454]},{"center":[20314.16816807454,44790.25575110297]},{"center":[22037.07201026651,45788.930285860224]},{"center":[23195.839390726658,44447.1996531682]}],"center":[21431.293347696246,44547.354112399335]}]}
-"""
+var colorData: [Color: UIColor] {
+    return selectedTheme.value == .dark ? Singleton.darkTheme : Singleton.lightTheme
+}
+var backgroundColorData: UIColor {
+    return selectedTheme.value == .dark ? Singleton.darkBackGround : Singleton.lightBackGround
+}
+var selectedUIColor: UIColor {
+    return (colorData[selectedColor.value])!
+}
