d070d565fb5ed863c23e1722cd371dd24d52f254
Parshav <parshav@live.com>
2018-05-27 11:25:37 -0500

    Added 4 digit id to cluster instead

--- NoteMap/Shared/Collab.swift
+++ NoteMap/Shared/Collab.swift
@@ -14,16 +14,16 @@ class Collab {
     
     private var ref: DatabaseReference = Database.database().reference()
     private var disposeBag = DisposeBag()
+    private let ID = 4.generateRandomNumber()
     
-    // Poor test of dbRef
+    // First call for setting up db ref
     func bindCluster(cluster: Cluster) {
-        self.ref.child("Cluster").setValue(cluster.id)
         cluster.notePanObservable.do(onSubscribe: {
-            self.ref.child("Cluster").child("Note").setValue("1")
+            self.ref.child("\(self.ID)")
         }).subscribe(onNext: { _ in
             
             let modelDict = self.clusterToClusterModelJson(cl: cluster).convertToDictionary()            
-            self.ref.child("Cluster").child("\(cluster.id)").setValue(modelDict)
+            self.ref.child("\(self.ID)").setValue(modelDict)
 
         }).disposed(by: disposeBag)
     }
@@ -35,6 +35,19 @@ class Collab {
     
     func dispose() {
         disposeBag = DisposeBag()
+    }
+}
+
+extension Int {
+    func generateRandomNumber() -> Int {
+        var place = 1
+        var finalNumber = 0
+        for _ in 0..<self {
+            place *= 10
+            let randomNumber = arc4random_uniform(10)
+            finalNumber += Int(randomNumber) * place
+        }
+        return finalNumber
     }
 }
 
